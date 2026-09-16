0882ac80c31529f024b95a73a02969d7c3fcd79a
Ogya <ogyarjndra@gmail.com>
2026-09-16 21:03:42 +0700

    fix bug banyak

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -1,10 +1,18 @@
 import multer from 'multer'
+import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const uploadDirectory = path.join(__dirname, '../../uploads')
+
+fs.mkdirSync(uploadDirectory, { recursive: true })
 
 // Konfigurasi tempat simpan dan nama file
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, uploadDirectory)
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
